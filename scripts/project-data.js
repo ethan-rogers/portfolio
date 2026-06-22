60869fa939b2ff087b62e6983b8e35fec70c8517
Ethan Rogers <75224466+holt-rogers@users.noreply.github.com>
2026-06-22 13:19:27 -0400

    added garbage detective

--- scripts/project-data.js
+++ scripts/project-data.js
@@ -1,12 +1,14 @@
 export const projectTitles = [
-    { id: 0, title: 'Navy Missile Optimization', summary: 'A program that optimizes the ideal stage size for a multi-stage missile.'},
-    { id: 1, title: 'Motion Controlled Mouse', summary: 'A computer mouse controlled by rotational movement.'},
-    { id: 2, title: 'PenGuin', summary: 'A note taking app designed for STEM problem solving.'},
+    { id: 0, title: 'Trash Detective', summary: 'A trashcan that automatically sorts recyclables using computer vision. Made for Hoo Hacks 2026.'},
+    { id: 1, title: 'Navy Missile Optimization', summary: 'A program that optimizes the ideal stage size for a multi-stage missile.'},
+    { id: 2, title: 'Motion Controlled Mouse', summary: 'A computer mouse controlled by rotational movement.'},
+    { id: 3, title: 'PenGuin', summary: 'A note taking app designed for STEM problem solving.'},
 ];
 
 
 
 export const projectFiles = [
+    "trash-detective.html",
     "navy-missile-optimization.html",
     "motion-controlled-mouse.html",
     "penguin.html"
