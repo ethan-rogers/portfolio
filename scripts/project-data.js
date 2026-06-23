1e22d44a11525557e590d545b4bf44cbb2d3b3f3
Ethan Rogers <75224466+holt-rogers@users.noreply.github.com>
2026-06-23 13:42:37 -0400

    added biomed page and updated bio

--- scripts/project-data.js
+++ scripts/project-data.js
@@ -1,14 +1,16 @@
 export const projectTitles = [
-    { id: 0, title: 'Trash Detective', summary: 'A trashcan that automatically sorts recyclables using computer vision. Made for Hoo Hacks 2026.'},
-    { id: 1, title: 'Navy Missile Optimization', summary: 'A program that optimizes the ideal stage size for a multi-stage missile.'},
-    { id: 2, title: 'Motion Controlled Mouse', summary: 'A computer mouse controlled by rotational movement.'},
-    { id: 3, title: 'PenGuin', summary: 'A note taking app designed for STEM problem solving.'},
+    { id: 0, title: 'Trash Detective', summary: 'A trashcan that automatically sorts recyclables using computer vision. '},
+    { id: 1, title: 'Sensor System for Prosthetic Fitting', summary: 'A device intended to optimize prosthetic socket fit for amputees. '},
+    { id: 2, title: 'Navy Missile Optimization', summary: 'A program that optimizes the ideal stage size for a multi-stage missile.'},
+    { id: 3, title: 'Motion Controlled Mouse', summary: 'A computer mouse controlled by rotational movement.'},
+    { id: 4, title: 'PenGuin', summary: 'A note taking app designed for STEM problem solving.'},
 ];
 
 
 
 export const projectFiles = [
     "trash-detective.html",
+    "biomed.html",
     "navy-missile-optimization.html",
     "motion-controlled-mouse.html",
     "penguin.html"
