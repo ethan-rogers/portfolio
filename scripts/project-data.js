3fe81910923678ebd6ef13d245b85af3fb07a221
Ethan Rogers <75224466+holt-rogers@users.noreply.github.com>
2026-07-18 13:10:44 -0400

    added IR remote

--- scripts/project-data.js
+++ scripts/project-data.js
@@ -2,7 +2,7 @@ export const projectTitles = [
     { id: 0, title: 'Trash Detective', summary: 'A trashcan that automatically sorts recyclables using computer vision. '},
     { id: 1, title: 'Sensor System for Prosthetic Fitting', summary: 'A device intended to optimize prosthetic socket fit for amputees. '},
     { id: 2, title: 'Navy Missile Optimization', summary: 'A program that optimizes the ideal stage size for a multi-stage missile.'},
-    { id: 3, title: 'Motion Controlled Mouse', summary: 'A computer mouse controlled by rotational movement.'},
+    { id: 3, title: 'Universal Receiver', summary: 'Use any IR remote with your PC.'},
     { id: 4, title: 'PenGuin', summary: 'A note taking app designed for STEM problem solving.'},
 ];
 
@@ -12,6 +12,6 @@ export const projectFiles = [
     "trash-detective.html",
     "biomed.html",
     "navy-missile-optimization.html",
-    "motion-controlled-mouse.html",
+    "remote-mapper.html",
     "penguin.html"
 ]
