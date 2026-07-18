637f3d466153b2eabd8cbdb85f13244024ca6439
Ethan Rogers <75224466+holt-rogers@users.noreply.github.com>
2026-07-18 17:21:51 -0400

    fixed typos

--- scripts/project-data.js
+++ scripts/project-data.js
@@ -1,8 +1,8 @@
 export const projectTitles = [
     { id: 0, title: 'Trash Detective', summary: 'A trashcan that automatically sorts recyclables using computer vision. '},
-    { id: 1, title: 'Sensor System for Prosthetic Fitting', summary: 'A device intended to optimize prosthetic socket fit for amputees. '},
+    { id: 1, title: 'Universal Receiver', summary: 'Use any IR remote with your PC.'},
     { id: 2, title: 'Navy Missile Optimization', summary: 'A program that optimizes the ideal stage size for a multi-stage missile.'},
-    { id: 3, title: 'Universal Receiver', summary: 'Use any IR remote with your PC.'},
+    { id: 3, title: 'Sensor System for Prosthetic Fitting', summary: 'A device intended to optimize prosthetic socket fit for amputees. '},
     { id: 4, title: 'PenGuin', summary: 'A note taking app designed for STEM problem solving.'},
 ];
 
@@ -10,8 +10,8 @@ export const projectTitles = [
 
 export const projectFiles = [
     "trash-detective.html",
-    "biomed.html",
-    "navy-missile-optimization.html",
     "remote-mapper.html",
+    "navy-missile-optimization.html",
+    "biomed.html",
     "penguin.html"
 ]
