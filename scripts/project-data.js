661944d1d45f595967a45f64715fbbf7a1490801
Ethan Rogers <75224466+ethan-rogers@users.noreply.github.com>
2026-08-28 11:28:10 -0400

    added spartacus

--- scripts/project-data.js
+++ scripts/project-data.js
@@ -1,14 +1,16 @@
 export const projectTitles = [
-    { id: 0, title: 'Trash Detective', summary: 'A trashcan that automatically sorts recyclables using computer vision. '},
-    { id: 1, title: 'Universal Receiver', summary: 'Use any IR remote with your PC.'},
-    { id: 2, title: 'Navy Missile Optimization', summary: 'A program that optimizes the ideal stage size for a multi-stage missile.'},
-    { id: 3, title: 'Sensor System for Prosthetic Fitting', summary: 'A device intended to optimize prosthetic socket fit for amputees. '},
-    { id: 4, title: 'PenGuin', summary: 'A note taking app designed for STEM problem solving.'},
+    { id: 0, title: 'Spartacus', summary: 'A vertical spinner combat robot. '},
+    { id: 1, title: 'Trash Detective', summary: 'A trashcan that automatically sorts recyclables using computer vision. '},
+    { id: 2, title: 'Universal Receiver', summary: 'Use any IR remote with your PC.'},
+    { id: 3, title: 'Navy Missile Optimization', summary: 'A program that optimizes the ideal stage size for a multi-stage missile.'},
+    { id: 4, title: 'Sensor System for Prosthetic Fitting', summary: 'A device intended to optimize prosthetic socket fit for amputees. '},
+    { id: 5, title: 'PenGuin', summary: 'A note taking app designed for STEM problem solving.'},
 ];
 
 
 
 export const projectFiles = [
+    "spartacus.html",
     "trash-detective.html",
     "remote-mapper.html",
     "navy-missile-optimization.html",
